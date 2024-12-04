dc46f335d6836e12d605d546a7c6f19e6cc57640
Алексей <gradskovaleksej@gmail.com>
2024-12-04 21:15:56 +0300

    Инициализация проекта

---
+++ src/main/java/ru/gav/creditbank/deal/DealApplication.java
@@ -0,0 +1,14 @@
+package ru.gav.creditbank.deal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DealApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(DealApplication.class, args);
+    }
+
+}
